529fbb64980f3b1a10ec56ccee2b61e13050a50e
Алексей Шлайков <shlaikov@1mps.ru>
2017-11-02 14:37:55 +0300

    First stable version.

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
+var debug = require('debug')('io');
+
 var express = require('express'),
-    http    = require('http'),
     path    = require('path');
 
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5050));
 app.set('appDir', path.dirname(require.main.filename));
 
-var server = http.createServer(app);
-var io = require('socket.io').listen(server.listen(app.get('port')));
+var io = require('socket.io').listen(app.listen(app.get('port')));
 
 require('./lib/config')(app, io);
 require('./lib/routes')(app, io);
 
-server.listen(app.get('port'), function() {
-  console.log('NodeJS app is running on port', app.get('port'));
-});
+console.log('Your messenger is running on http://localhost:' + app.get('port'));
